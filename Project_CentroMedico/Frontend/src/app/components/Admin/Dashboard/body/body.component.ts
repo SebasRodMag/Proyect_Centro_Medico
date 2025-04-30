70b64b6d11b186e408691fc0526564da5e0a5a5e
brandonlr01 <brandonlorenzo1daw@gmail.com>
2025-04-30 12:05:09 +0200

    Vista hecha, sin funcionalidad, de Dashboard Admin / Sección Clientes

--- Project_CentroMedico/Frontend/src/app/components/Admin/Dashboard/body/body.component.ts
+++ Project_CentroMedico/Frontend/src/app/components/Admin/Dashboard/body/body.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavComponent } from './nav/nav.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
 
 @Component({
   selector: 'app-body',
   imports: [
-    NavComponent, ClientesComponent
+    NavComponent, ClientesComponent, UsuariosComponent
   ],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
---
+++ Project_CentroMedico/Frontend/src/app/components/Admin/Dashboard/body/citas/modal-create/modal-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-create',
+  imports: [],
+  templateUrl: './modal-create.component.html',
+  styleUrl: './modal-create.component.css'
+})
+export class ModalCreateComponent {
+
+}
