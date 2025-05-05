7b30a1309bb90f3ce68892fab24e231af66cd912
brandonlr01 <brandonlorenzo1daw@gmail.com>
2025-05-05 09:15:53 +0200

    Correción del Frontend

--- Project_CentroMedico/Frontend/src/app/components/Admin/Dashboard/body/body.component.ts
+++ Project_CentroMedico/Frontend/src/app/components/Admin/Dashboard/body/body.component.ts
@@ -2,16 +2,16 @@ import { Component } from '@angular/core';
 import { NavComponent } from './nav/nav.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
+import { MedicosComponent } from './medicos/medicos.component';
 
 @Component({
   selector: 'app-body',
   imports: [
-    NavComponent, ClientesComponent, UsuariosComponent
+    NavComponent, /*ClientesComponent,*/ /*UsuariosComponent,*/ MedicosComponent
   ],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
 export class BodyComponent {
 
-}
 }
