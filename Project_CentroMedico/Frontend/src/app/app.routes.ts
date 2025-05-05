68896126149d0babd3282a695d476eb374fba9e1
brandonlr01 <brandonlorenzo1daw@gmail.com>
2025-05-05 13:28:42 +0200

    Rutas de componentes

--- Project_CentroMedico/Frontend/src/app/app.routes.ts
+++ Project_CentroMedico/Frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { CitasComponent } from './components/Admin/Dashboard/body/citas/citas.component';
+import { ClientesComponent } from './components/Admin/Dashboard/body/clientes/clientes.component';
+import { MedicosComponent } from './components/Admin/Dashboard/body/medicos/medicos.component';
+import { UsuariosComponent } from './components/Admin/Dashboard/body/usuarios/usuarios.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'admin', pathMatch: 'full'},
+    { path: 'citas', component: CitasComponent},
+    { path: 'clientes', component: ClientesComponent},
+    { path: 'medicos', component: MedicosComponent},
+    { path: 'usuarios', component: UsuariosComponent},
+];
