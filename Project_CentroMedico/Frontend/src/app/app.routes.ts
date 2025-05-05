b4e2bcb0b99740148095650efec444eaae4bcb61
Sebastián Rodríguez <153898337+SebasRodMag@users.noreply.github.com>
2025-05-05 15:20:24 +0200

    Se crea los componentes Home y Login. Se instala el enrutador de angular

--- Project_CentroMedico/Frontend/src/app/app.routes.ts
+++ Project_CentroMedico/Frontend/src/app/app.routes.ts
@@ -1,3 +1,34 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { CitasComponent } from '../app/components/Admin/Dashboard/body/citas/citas.component';
+import { ClientesComponent } from '../app/components/Admin/Dashboard/body/clientes/clientes.component';
+import { MedicosComponent } from '../app/components/Admin/Dashboard/body/medicos/medicos.component';
+import { PacientesComponent } from '../app/components/Admin/Dashboard/body/pacientes/pacientes.component';
+import { CardsComponent } from '../app/components/Admin/Dashboard/body/cards/cards.component';
+import { NavComponent } from '../app/components/Admin/Dashboard/body/nav/nav.component';
+import { BodyComponent } from '../app/components/Admin/Dashboard/body/body.component';
+
+// Para cuando implementemos el Home y Login
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent }, // Página principal
+    { path: 'login', component: LoginComponent },
+    { path: 'citas', component: CitasComponent },
+    { path: 'clientes', component: ClientesComponent },
+    { path: 'medicos', component: MedicosComponent },
+    { path: 'pacientes', component: PacientesComponent },
+    { path: 'cards', component: CardsComponent },
+    { path: 'nav', component: NavComponent },
+    { path: 'body', component: BodyComponent },
+    { path: '**', redirectTo: '' }, // Redirección a Home si no se encuentra la ruta
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
+
--- Project_CentroMedico/Frontend/src/app/components/Admin/Dashboard/body/clientes/clientes.component.ts
+++ Project_CentroMedico/Frontend/src/app/components/Admin/Dashboard/body/clientes/clientes.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
-@Component({
-  selector: 'app-clientes',
-  imports: [],
 import { ModalCreateComponent } from './modal-create/modal-create.component';
 
 @Component({
   selector: 'app-clientes',
+  standalone: true,
   imports: [
     ModalCreateComponent
   ],
   templateUrl: './clientes.component.html',
-  styleUrl: './clientes.component.css'
+  styleUrls: ['./clientes.component.css']
 })
 export class ClientesComponent {
 
