541e0a1533f111c9650a547b10da93ca76c0dd37
brandonlr01 <brandonlorenzo1daw@gmail.com>
2025-05-06 08:49:59 +0200

    Login y Home

--- Project_CentroMedico/Frontend/src/app/app.component.ts
+++ Project_CentroMedico/Frontend/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/Admin/Dashboard/header/header.component';
 import { BodyComponent } from './components/Admin/Dashboard/body/body.component';
+import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, BodyComponent],
+  imports: [RouterOutlet, HeaderComponent, BodyComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
