4013d8e69e2838133f73386dbc2861beb7ddc5b7
Sebastián Rodríguez <153898337+SebasRodMag@users.noreply.github.com>
2025-05-11 19:44:36 +0200

    Comprobación de la estructura de la base de datos

--- Project_CentroMedico/Backend/database/seeders/UserSeeder.php
+++ Project_CentroMedico/Backend/database/seeders/UserSeeder.php
@@ -36,7 +36,7 @@ class UserSeeder extends Seeder
         }
 
         // Crear usuarios clientes
-        for ($i = 1; $i <= 5; $i++) {
+        for ($i = 1; $i <= 10; $i++) {
             $cliente = User::create([
                 'email' => "cliente{$i}@example.com",
                 'password' => Hash::make('password123'),
@@ -45,7 +45,7 @@ class UserSeeder extends Seeder
         }
 
         // Crear usuarios pacientes
-        for ($i = 1; $i <= 5; $i++) {
+        for ($i = 1; $i <= 60; $i++) {
             $paciente = User::create([
                 'email' => "paciente{$i}@example.com",
                 'password' => Hash::make('password123'),
